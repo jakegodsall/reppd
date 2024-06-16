6e5da2f2d3d66b6fed528b2c8eb54d34b7209172
Jake Godsall <jake.edward.godsall@gmail.com>
2024-06-16 14:53:52 +0100

    implemented custom response body for MethodArgumentNotValidException

--- src/main/java/org/jakegodsall/reppd/controllers/ExceptionController.java
+++ src/main/java/org/jakegodsall/reppd/controllers/ExceptionController.java
@@ -2,8 +2,13 @@ package org.jakegodsall.reppd.controllers;
 
 import org.jakegodsall.reppd.exceptions.NotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 public class ExceptionController {
@@ -11,5 +16,18 @@ public class ExceptionController {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<Void> handleNotFoundException() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<Map<String, String>>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+
+        List<Map<String, String>> fieldErrors = exception.getBindingResult().getFieldErrors().stream()
+                .map(fieldError -> {
+                    Map<String, String> errorMap = new HashMap<>();
+                    errorMap.put(fieldError.toString(), fieldError.getDefaultMessage());
+                    return errorMap;
+                }).toList();
+
+        return ResponseEntity.badRequest().body(fieldErrors);
     }
 }
