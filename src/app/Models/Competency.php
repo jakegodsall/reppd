c5106091df6304ceb6790870e846544020e87326
Jake Godsall <jake.edward.godsall@gmail.com>
2024-12-08 19:48:15 +0000

    added model properties

--- src/app/Models/Competency.php
+++ src/app/Models/Competency.php
@@ -1,12 +1,34 @@
 <?php
 
+declare(strict_types=1)
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Competency extends Model
 {
-    /** @use HasFactory<\Database\Factories\CompetencyFactory> */
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'id',
+        'title',
+        'description',
+        'status',
+        'start_date',
+        'end_date',
+        'user_id',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -1,9 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -44,5 +47,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function competencies(): HasMany
+    {
+        return $this->hasMany(Competency::class);
     }
 }
